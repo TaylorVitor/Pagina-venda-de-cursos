ac80b59852db8f8daf40ec875d077cf322ae4700
Taylor Vitor <taylorvitor34@gmail.com>
2022-12-01 15:53:49 -0300

    Atualização do projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+
+
+let botaoMais = document.querySelector(".maisCursos")
+
+let box2 = document.querySelector(".box2")
+
+botaoMais.addEventListener("click", () => {
+    box2.classList.add("mais")
+
+    botaoMais.classList.add("sumir")
+})
+
+
+
+
+
